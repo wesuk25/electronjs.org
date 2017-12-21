16445f471866ae152f367f3d090b355a4beb3e5c
Pierre Neter <pierreneter@gmail.com>
2017-12-22 03:33:17 +0700

    Translate doc

--- routes/docs/show.js
+++ routes/docs/show.js
@@ -1,4 +1,6 @@
 const i18n = require('../../lib/i18n')
+const getIds = require('get-crowdin-file-ids')
+const { findKey } = require('lodash')
 
 module.exports = (req, res, next) => {
   const doc = i18n.docs[req.language][req.path]
@@ -13,6 +15,19 @@ module.exports = (req, res, next) => {
     },
     layout: 'docs'
   })
+
+  doc.linkToTranslate = 'https://crowdin.com/project/electron'
+  if (req.context.currentLocale !== 'en-US') {
+    if (process.env.CROWDIN_KEY) {
+      let listIds = getIds('electron', process.env.CROWDIN_KEY)
+      let id = listIds[`master/content/en-US${doc.href}.md`]
+      if (id !== undefined) {
+        doc.linkToTranslate = `https://crowdin.com/translate/electron/${id}/en-${
+          req.context.currentLocale.replace(/-/g, '')
+        }`
+      }
+    }
+  }
 
   res.render('docs/show', context)
 }
